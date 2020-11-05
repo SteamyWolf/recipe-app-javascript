1fe59c0a6916cde8ab508672984190751890cf7e
Wyatt <wyatthunterallan@gmail.com>
2020-11-04 17:03:30 -0700

    Successfully got ingredients by recipe id

--- backend/models/ingredientModel.js
+++ backend/models/ingredientModel.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 
 const ingredientSchema = mongoose.Schema({
     name: String,
-    amount: Number
+    amount: Number,
+    recipeID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Recipe'
+    }
 })
 
 module.exports = mongoose.model('Ingredient', ingredientSchema, 'ingredients');
--- backend/routes/ingredientRoute.js
+++ backend/routes/ingredientRoute.js
@@ -17,13 +17,27 @@ router.post('/', async (req, res) => {
     try {
         const ingredient = new Ingredient({
             name: req.body.name,
-            amount: req.body.amount
+            amount: req.body.amount,
+            recipeID: req.body.recipeID
         })
         const saveIngredient = await ingredient.save();
         res.json(saveIngredient);
     }
     catch(err) {
         console.log(err)
+    }
+})
+
+router.get('/ingredientsByRecipe', async (req, res) => {
+    console.log(req.body)
+    try {
+        await Ingredient.find({ recipeID: req.body.recipeID })
+        // productByManufac.populate('products')
+        .exec((err, ingredients) => {
+            res.json(ingredients)
+        })
+    } catch(err) {
+        res.json(err)
     }
 })
 
