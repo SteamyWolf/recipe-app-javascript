e295fad582d50aaf2c7d81d8583364888fdba15b
Wyatt <wyatthunterallan@gmail.com>
2020-11-06 16:03:27 -0700

    Categories click feature works and it displays the correct recipes with their ingredients. Clearing of the childNodes when a new click occurs

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 const categoriesList = document.querySelector('.categories-list');
 const ulOfCategories = document.querySelector('.li-list-category');
+const recipeListDiv = document.querySelector('.recipes-list');
+
 
 async function getRecipes() {
     const response = await fetch('http://localhost:3000/recipes', {
@@ -13,8 +15,8 @@ async function getRecipes() {
 
 getRecipes().then(recipes => {
     console.log(recipes)
-    displayCategories(recipes)
-    let combined = recipes.map(recipe => {
+    
+    let recipesWithIngredients = recipes.map(recipe => {
         fetch(`http://localhost:3000/ingredients/ingredientsByRecipe/${recipe._id}`, {
             method: 'GET',
             headers: { 'Access-Control-Allow-Orgin': 'Content-Type', 'Content-Type': 'application/json' },
@@ -25,19 +27,85 @@ getRecipes().then(recipes => {
         })
         return recipe
     })
-    console.log(combined)
+    console.log(recipesWithIngredients)
+    displayCategories(recipesWithIngredients)
+    // displayRecipes(recipesWithIngredients)
 })
 
-function displayCategories(recipes) {
-    let finalCategories = [];
-    let categories = new Set(recipes.map(recipe => recipe.category))
+function displayCategories(recipesWithIngredients) {
+    let categories = new Set(recipesWithIngredients.map(recipe => recipe.category))
     console.log(categories)
     categories.forEach(category => {
         ulOfCategories.appendChild(createCategory(category))
-    })
+    });
+    categoryClick(recipesWithIngredients);
 }
 function createCategory(category) {
     let li = document.createElement('li')
+    li.className = 'category-item'
     li.textContent = category
     return li
+}
+
+//Event Listeners
+function categoryClick(recipesWithIngredients) {
+    const categoryItem = document.querySelectorAll('.category-item');
+    categoryItem.forEach(category => {
+        category.addEventListener('click', (event) => {
+            const indexOfElement = [...event.target.parentElement.children].indexOf(event.target)
+            const categoryName = event.target.innerHTML
+            displayRecipes(recipesWithIngredients, categoryName)
+        })
+    })
+    
+}
+
+
+function displayRecipes(recipesWithIngredients, categoryName) {
+    // if (recipeListDiv.hasChildNodes()) {
+    //     recipeListDiv.removeChild()
+    // }
+    while (recipeListDiv.firstChild) {
+        recipeListDiv.removeChild(recipeListDiv.firstChild)
+    }
+    recipesWithIngredients.forEach(recipe => {
+        if (recipe.category === categoryName) {
+            let recipeDiv = document.createElement('div')
+            recipeDiv.className = 'recipe';
+            let recipeHeaderDiv = document.createElement('div')
+            recipeHeaderDiv.className = 'recipe-header';
+            let h2 = document.createElement('h2');
+            let p = document.createElement('p');
+            h2.textContent = recipe.title;
+            p.textContent = 'Rating: ' + recipe.rating + '/5';
+            recipeHeaderDiv.appendChild(h2);
+            recipeHeaderDiv.appendChild(p);
+
+            let pDescription = document.createElement('p')
+            pDescription.className = 'description'
+            pDescription.textContent = recipe.directions
+            recipeDiv.appendChild(recipeHeaderDiv)
+            recipeDiv.appendChild(pDescription)
+
+            let ingredientListDiv = document.createElement('div')
+            ingredientListDiv.className = 'ingredient-list'
+
+            let ingredientH3 = document.createElement('h3')
+            ingredientH3.textContent = 'Ingredients:'
+
+            let liListIngredientDiv = document.createElement('div')
+            liListIngredientDiv.className = 'li-list-ingredient'
+            recipe.ingredients[0].forEach(ingredient => {
+                let liIngredient = document.createElement('li')
+                liIngredient.textContent = ingredient.name + ' - ' + ingredient.amount
+                liListIngredientDiv.appendChild(liIngredient);
+            })
+            ingredientListDiv.appendChild(ingredientH3)
+            ingredientListDiv.appendChild(liListIngredientDiv)
+            recipeDiv.appendChild(ingredientListDiv)
+            
+            recipeListDiv.appendChild(recipeDiv)
+        }
+        
+    })
 }
