5bf404be83b96f793e1f964f5981bff767454dc1
Wyatt <wyatthunterallan@gmail.com>
2020-11-06 17:02:34 -0700

    Began work on all the recipes being displayed on page load

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 const categoriesList = document.querySelector('.categories-list');
 const ulOfCategories = document.querySelector('.li-list-category');
 const recipeListDiv = document.querySelector('.recipes-list');
-
+const allRecipes = document.querySelector('.allRecipes');
 
 async function getRecipes() {
     const response = await fetch('http://localhost:3000/recipes', {
@@ -29,7 +29,7 @@ getRecipes().then(recipes => {
     })
     console.log(recipesWithIngredients)
     displayCategories(recipesWithIngredients)
-    // displayRecipes(recipesWithIngredients)
+    allRecipesClick(recipesWithIngredients)
 })
 
 function displayCategories(recipesWithIngredients) {
@@ -59,53 +59,66 @@ function categoryClick(recipesWithIngredients) {
     })
     
 }
+function allRecipesClick(recipesWithIngredients) {
+    allRecipes.addEventListener('click', event => {
+        console.log(event)
+        displayRecipes(recipesWithIngredients, '');
+    })
+}
+
 
 
 function displayRecipes(recipesWithIngredients, categoryName) {
-    // if (recipeListDiv.hasChildNodes()) {
-    //     recipeListDiv.removeChild()
-    // }
     while (recipeListDiv.firstChild) {
         recipeListDiv.removeChild(recipeListDiv.firstChild)
     }
     recipesWithIngredients.forEach(recipe => {
         if (recipe.category === categoryName) {
-            let recipeDiv = document.createElement('div')
-            recipeDiv.className = 'recipe';
-            let recipeHeaderDiv = document.createElement('div')
-            recipeHeaderDiv.className = 'recipe-header';
-            let h2 = document.createElement('h2');
-            let p = document.createElement('p');
-            h2.textContent = recipe.title;
-            p.textContent = 'Rating: ' + recipe.rating + '/5';
-            recipeHeaderDiv.appendChild(h2);
-            recipeHeaderDiv.appendChild(p);
-
-            let pDescription = document.createElement('p')
-            pDescription.className = 'description'
-            pDescription.textContent = recipe.directions
-            recipeDiv.appendChild(recipeHeaderDiv)
-            recipeDiv.appendChild(pDescription)
-
-            let ingredientListDiv = document.createElement('div')
-            ingredientListDiv.className = 'ingredient-list'
-
-            let ingredientH3 = document.createElement('h3')
-            ingredientH3.textContent = 'Ingredients:'
-
-            let liListIngredientDiv = document.createElement('div')
-            liListIngredientDiv.className = 'li-list-ingredient'
-            recipe.ingredients[0].forEach(ingredient => {
-                let liIngredient = document.createElement('li')
-                liIngredient.textContent = ingredient.name + ' - ' + ingredient.amount
-                liListIngredientDiv.appendChild(liIngredient);
-            })
-            ingredientListDiv.appendChild(ingredientH3)
-            ingredientListDiv.appendChild(liListIngredientDiv)
-            recipeDiv.appendChild(ingredientListDiv)
-            
-            recipeListDiv.appendChild(recipeDiv)
+            createRecipe(recipe)
         }
-        
     })
+    if (!categoryName) {
+        recipesWithIngredients.forEach(recipe => {
+            createRecipe(recipe)
+        })
+    }
+}
+
+function createRecipe(recipe) {
+    console.log(recipe)
+    let recipeDiv = document.createElement('div')
+    recipeDiv.className = 'recipe';
+    let recipeHeaderDiv = document.createElement('div')
+    recipeHeaderDiv.className = 'recipe-header';
+    let h2 = document.createElement('h2');
+    let p = document.createElement('p');
+    h2.textContent = recipe.title;
+    p.textContent = 'Rating: ' + recipe.rating + '/5';
+    recipeHeaderDiv.appendChild(h2);
+    recipeHeaderDiv.appendChild(p);
+
+    let pDescription = document.createElement('p')
+    pDescription.className = 'description'
+    pDescription.textContent = recipe.directions
+    recipeDiv.appendChild(recipeHeaderDiv)
+    recipeDiv.appendChild(pDescription)
+
+    let ingredientListDiv = document.createElement('div')
+    ingredientListDiv.className = 'ingredient-list'
+
+    let ingredientH3 = document.createElement('h3')
+    ingredientH3.textContent = 'Ingredients:'
+
+    let liListIngredientDiv = document.createElement('div')
+    liListIngredientDiv.className = 'li-list-ingredient'
+    recipe.ingredients[0].forEach(ingredient => {
+        let liIngredient = document.createElement('li')
+        liIngredient.textContent = ingredient.name + ' - ' + ingredient.amount
+        liListIngredientDiv.appendChild(liIngredient);
+    })
+    ingredientListDiv.appendChild(ingredientH3)
+    ingredientListDiv.appendChild(liListIngredientDiv)
+    recipeDiv.appendChild(ingredientListDiv)
+    
+    recipeListDiv.appendChild(recipeDiv)
 }
